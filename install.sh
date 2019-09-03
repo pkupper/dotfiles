dad90b9ba8f3ab5861c9e6bda7634bf8dee70858
Paul Kupper <kupper.pa@gmail.com>
2019-09-03 21:31:36 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ln -s $(pwd)/config/rofi ~/.config/
+ln -s $(pwd)/config/sway ~/.config/
+ln -s $(pwd)/config/swaylock ~/.config/
+ln -s $(pwd)/config/waybar ~/.config/
+
+ln -s $(pwd)/shell/zsh/.zshrc ~
+ln -s $(pwd)/shell/.profile ~/.zprofile
