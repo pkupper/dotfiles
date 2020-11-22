d6c94440aca4e908b48f84bee3c51741f2b01a28
Paul Kupper <kupper.pa@gmail.com>
2020-11-22 23:08:54 +0100

    add wofi config

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -s $(pwd)/config/rofi ~/.config/
 ln -s $(pwd)/config/sway ~/.config/
 ln -s $(pwd)/config/swaylock ~/.config/
 ln -s $(pwd)/config/waybar ~/.config/
+ln -s $(pwd)/config/wofi ~/.config/
 
 ln -s $(pwd)/shell/zsh/.zshrc ~
 ln -s $(pwd)/shell/.profile ~/.zprofile
